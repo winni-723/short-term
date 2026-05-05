fa5513abbabef40d9fe51452f0a8847e6ecfd9a1
Winni <winnie1238888@gmail.com>
2026-05-05 03:24:34 -0400

    set icon with png

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "short-term-investment-v36";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+const CACHE_NAME = "short-term-investment-v37";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg", "./icon-180.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
